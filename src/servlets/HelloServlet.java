7c3fb2b1667d170a601684934b2e5dc2faee0851
Sachin Devasahayam <sjd232@cornell.edu>
2020-03-31 20:43:55 -0400

    Created servlet

---
+++ src/servlets/HelloServlet.java
@@ -0,0 +1,35 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+//This annotation means that this servlet handles requests with "/helloServlet" tag
+//For example, index.jsp has a form with action = "helloServlet" which means when that form is submitted
+//This servlet processes it
+@WebServlet("/helloServlet") 
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
